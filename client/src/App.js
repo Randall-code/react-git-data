815736236352ab74b5712f425ebce03b4463cdf5
Oliver Randall <ollie@Olivers-MacBook-Pro.local>
2023-01-01 16:53:33 +0000

    adds ability to search name of github user

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect, useRef } from "react"
+
+function UserData({user}) {
+ 
+  return(
+    <p>{user.name}</p>
+
+  );
+}
+
+
+
 
 function App() {
+
+  const inputRef = useRef(null);
+
+  const [githubUser, setGithubUser] = useState(null);
+  const [githubUserData, setGithubUserData] = useState(null);
+  
+
+  const handleClick = () => {
+    // 👇 "inputRef.current.value" is input value
+    setGithubUser(inputRef.current.value);
+  }; 
+  
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${githubUser}`)
+      .then((res) => {
+        if(!res.ok) throw new Error(res.status)
+        else return res.json()
+      })
+      .then(data => setGithubUserData(data))
+      .catch((error) => {
+        console.log('error: ' + error);
+      });
+  }, [githubUser]);
+
+  console.log(githubUser);
+  console.log(githubUserData);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        ref={inputRef}
+        type="text"
+        id="message"
+        name="message"
+      />
+
+      <button onClick={handleClick}>Update</button>
+        
+      {/* < UserData user={githubUSer} /> */}
+      {githubUserData ? < UserData user={githubUserData} /> : null}
     </div>
   );
 }
